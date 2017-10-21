c343fff86a44f8021020b7addcd26dd0d85a8b1c
Daniel O <32978956+NeoGlitter@users.noreply.github.com>
2017-10-22 06:38:50 +1000

    Initial TeleOP steering OP, with some modifications to variable in Hardware definition class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareBot.java
@@ -26,16 +26,20 @@ public class HardwareBot
     /* Public OpMode members. */
     public DcMotor  leftDrive   = null;
     public DcMotor  rightDrive  = null;
-    public DcMotor  leftArm     = null;
+    public DcMotor  arm         = null;
+    public DcMotor  linearDrive = null;
     public Servo    leftClaw    = null;
     public Servo    rightClaw   = null;
+    public Servo    jewelArm    = null;
 
-    public static final double MID_SERVO       =  0.5 ;
-    public static final double ARM_UP_POWER    =  0.45 ;
-    public static final double ARM_DOWN_POWER  = -0.45 ;
+    public static final double LEFT_CLAW_OPEN       =  0.05;
+    public static final double RIGHT_CLAW_OPEN      =  0.85;
+    public static final double JEWEL_ARM_UP         =  0.9;
+
+
 
     /* local OpMode members. */
-    HardwareMap hwMap           =  null;
+    HardwareMap hwMap           = null;
     private ElapsedTime period  = new ElapsedTime();
 
     /* Constructor */
@@ -48,29 +52,31 @@ public class HardwareBot
         // Save reference to Hardware map
         hwMap = ahwMap;
 
-        // Define and Initialize Motors
-        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
-        rightDrive = hwMap.get(DcMotor.class, "right_drive");
-        leftArm    = hwMap.get(DcMotor.class, "left_arm");
-        leftDrive.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        // Define and Initialize DC Motors
+        leftDrive   = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive  = hwMap.get(DcMotor.class, "right_drive");
+        arm         = hwMap.get(DcMotor.class, "left_arm");
+        linearDrive = hwMap.get(DcMotor.class, "linear_drive");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Set all motors to zero power
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        leftArm.setPower(0);
+        arm.setPower(0);
 
-        // Set all motors to run without encoders.
-        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        // Set all motors to run without encoders, as they are not currently installed and connected
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        leftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         // Define and initialize ALL installed servos.
-        leftClaw  = hwMap.get(Servo.class, "left_hand");
-        rightClaw = hwMap.get(Servo.class, "right_hand");
-        leftClaw.setPosition(MID_SERVO);
-        rightClaw.setPosition(MID_SERVO);
+        leftClaw  = hwMap.get(Servo.class, "left_claw");
+        rightClaw = hwMap.get(Servo.class, "right_claw");
+        jewelArm  = hwMap.get(Servo.class, "jewel_arm");
+        leftClaw.setPosition(LEFT_CLAW_OPEN);
+        rightClaw.setPosition(RIGHT_CLAW_OPEN);
+        jewelArm.setPosition(JEWEL_ARM_UP);
     }
  }
 
